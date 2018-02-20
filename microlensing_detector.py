0fca37bb48445ded836e11cc3f1ed9180038a4f6
joshcalcino <josh.calcino@gmail.com>
2018-02-20 19:45:31 +1000

    Moved functions that are used in multiple modules

---
+++ microlensing_detector.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+class DetectMicrolensingEvents(object):
+    """ Detect microlensing events which are stored in a dictionary. """
+
+    def __init__(self):
+        pass
+
+    def variability_cut(self):
+        pass
+
+    def achromaticity_cut(self):
+        pass
